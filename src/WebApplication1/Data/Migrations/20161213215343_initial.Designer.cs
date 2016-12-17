f0a86553c84306721c346befd513d43481a14b0e
mansoork <mansoor_ak@outlook.com>
2016-12-17 15:31:23 +0530

    Practice code

--- src/WebApplication1/Data/Migrations/20161213215343_initial.Designer.cs
+++ src/WebApplication1/Data/Migrations/20161213215343_initial.Designer.cs
@@ -8,9 +8,10 @@ using WebApplication1.Data;
 namespace WebApplication1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161213215343_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
